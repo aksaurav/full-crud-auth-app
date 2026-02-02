161b6cd55c46fd2f3cf937b1f2c9d31d42196cc3
Abdul Kadir <aksaurav1999@gmail.com>
2026-02-02 14:59:13 -0500

    Fix: Configure CORS for local and production origins

--- Backend/server.js
+++ Backend/server.js
@@ -6,8 +6,28 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const allowedOrigins = [
+  `http://localhost:5173`,
+  `https://mern-crud-auth-app.onrender.com`,
+];
+
 // Middlewares
-app.use(cors({}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 // Mongo DB connections
